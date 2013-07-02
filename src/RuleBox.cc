58f505d8d14c7b05164d2a313d071d608fc176a4
Mateusz Lis <mateusz.lis@gmail.com>
2013-07-03 00:32:11 +0200

    [REFAC] Split of process into subfunctions

--- src/RuleBox.cc
+++ src/RuleBox.cc
@@ -13,11 +13,28 @@ void RuleBox::addRule( const IRule::Ptr& aRule )
     m_Rules.push_front( aRule );
 }
 
-std::string RuleBox::process( const std::string& aText, const uint64_t aLineNumber )
+std::string RuleBox::process( const std::string& aText
+        , const uint64_t aLineNumber ) const
+{
+    auto lIResult( applyRules( aText, aLineNumber ) );
+    std::stringstream lOutput;
+    std::list< ColorName > lColors;
+    ColorName lCurrentColor( RESET );
+    for ( size_t lIndex( 0 ) ; lIndex < aText.size() ; ++lIndex )
+    {
+        processLetter( lIndex, lColors, lOutput, lIResult, lCurrentColor );
+        lOutput << aText[ lIndex ];
+    }
+    if ( !lColors.empty() ) // loop might omit last sign
+        lOutput << COLOR_NAMES[ RESET ];
+    return lOutput.str();
+}
+
+IntermediateResult RuleBox::applyRules( const std::string& aText
+                , const size_t aLineNumber ) const
 {
     IntermediateResult lIResult( aText.size() );
-    std::function< void( const IRule::Ptr& ) >
-        lProcessor ( std::bind( &RuleBox::singleProcess
+    auto lProcessor ( std::bind( &RuleBox::singleProcess
                     , this
                     , std::placeholders::_1
                     , std::ref( lIResult )
@@ -25,71 +42,45 @@ std::string RuleBox::process( const std::string& aText, const uint64_t aLineNumb
                     , aLineNumber )
                    );
     std::for_each( begin( m_Rules ), end( m_Rules ), lProcessor );
-
-    std::stringstream lOutput;
-    std::list< ColorName > lColors;
-    ColorName lCurrentColor( RESET );
-    for ( size_t lIndex( 0 ) ; lIndex < aText.size() ; ++lIndex )
-    {
-        IntermediateResult::Markers lMarkers;
-        lIResult.getMarkers( lIndex, lMarkers );
-        bool lReset( false );
-        if ( !lMarkers.empty() )
-        {
-            for ( size_t lRuleIndex( 0 ) ; lRuleIndex < lMarkers.size() ; ++lRuleIndex )
-            {
-                if ( !lMarkers[ lRuleIndex ].first )
-                {
-                    lColors.push_front( lMarkers[ lRuleIndex ].second );
-                }
-                else
-                {
-                    if ( lColors.front() == lMarkers[ lRuleIndex ].second )
-                    {
-                        lColors.pop_front(); // ninenty percent of the time
-                    }
-                    else
-                    {
-                        removeFirstOccurence( lMarkers[ lRuleIndex ].second
-                                , lColors );
-                    }
-                    if ( lColors.empty() )
-                    {
-                        lReset = true;
-                        lCurrentColor = RESET;
-                    }
-                }
-            }
-            if ( lReset )
-            {
-                lOutput << COLOR_NAMES[ RESET ];
-                lReset = false;
-            }
-            else if ( lCurrentColor != lColors.front() )
-            {
-                lOutput << COLOR_NAMES[ lColors.front() ];
-            }
-            lCurrentColor = lColors.front();
-        }
-        lOutput << aText[ lIndex ];
-    }
-    if ( !lColors.front() )
-        lOutput << COLOR_NAMES[ RESET ];
-    return lOutput.str();
+    return lIResult;
 }
 
 void RuleBox::singleProcess( const IRule::Ptr& aRule
                             , IntermediateResult& aResult
                             , const std::string& aLine
-                            , const uint64_t aLineNumber )
+                            , const uint64_t aLineNumber ) const
 {
     aRule->apply( aLine, aResult, aLineNumber );
 }
 
-void RuleBox::removeFirstOccurence( const ColorName aColor
-                            , std::list< ColorName >& aColors )
+void RuleBox::processLetter( const size_t aIndex
+                , std::list< ColorName >& aColors
+                , std::ostream& aOutput
+                , const IntermediateResult& aIResult
+                , ColorName& aCurrentColor ) const
 {
-    for ( std::list< ColorName >::iterator lColIt( aColors.begin() )
+    IntermediateResult::Markers lMarkers;
+    aIResult.getMarkers( aIndex, lMarkers );
+    bool lReset( false );
+    if ( !lMarkers.empty() )
+    {
+        auto lMarkerProcessor( 
+                std::bind( 
+                    &RuleBox::processMarkerForLetter
+                    , this
+                    , std::placeholders::_1
+                    , std::ref( aColors )
+                    , std::ref( lReset )
+                    , std::ref( aCurrentColor ) ) );
+        std::for_each( begin( lMarkers ), end( lMarkers ), lMarkerProcessor );
+        updateCurrentColor( aOutput, lReset, aCurrentColor, aColors.front() );
+    }
+}
+
+void RuleBox::removeFirstOccurence( const ColorName aColor
+                            , std::list< ColorName >& aColors ) const
+{
+    for ( auto lColIt( aColors.begin() )
             ; lColIt != aColors.end()
             ; ++lColIt )
     {
@@ -101,4 +92,42 @@ void RuleBox::removeFirstOccurence( const ColorName aColor
     }
 }
 
+void RuleBox::processMarkerForLetter( 
+                const IntermediateResult::Marker& aMarker
+                , std::list< ColorName >& aColors
+                , bool& aReset
+                , ColorName& aCurrentColor ) const
+{
+    if ( !aMarker.first )
+    {
+        aColors.push_front( aMarker.second );
+    }
+    else
+    {
+        removeFirstOccurence( aMarker.second
+                , aColors );
+        if ( aColors.empty() )
+        {
+            aReset = true;
+            aCurrentColor = RESET;
+        }
+    }
+}
+
+void RuleBox::updateCurrentColor( std::ostream& aOutput
+                , bool& aReset
+                , ColorName& aCurrentColor
+                , const ColorName& aTopColor ) const
+{
+    if ( aReset )
+    {
+        aOutput << COLOR_NAMES[ RESET ];
+        aReset = false;
+    }
+    else if ( aCurrentColor != aTopColor )
+    {
+        aOutput << COLOR_NAMES[ aTopColor ];
+    }
+    aCurrentColor = aTopColor;
+}
 } // namespace Color
--- src/RuleBox.hh
+++ src/RuleBox.hh
@@ -15,15 +15,32 @@ class RuleBox
     public: // functions
         RuleBox();
         void addRule( const IRule::Ptr& aRule );
-        std::string process( const std::string& aText, const uint64_t aLineNumber = 0 );
+        std::string process( const std::string& aText
+                , const uint64_t aLineNumber = 0 ) const;
 
-    protected: // functions
+    protected: // functions supporting processing
+        IntermediateResult applyRules( const std::string& aText
+                        , const size_t aLineNumber ) const;
         void singleProcess( const IRule::Ptr& aRule
                         , IntermediateResult& aResult
                         , const std::string& aLine
-                        , const uint64_t aLineNumber );
+                        , const uint64_t aLineNumber ) const;
+        void processLetter( const size_t aIndex
+                        , std::list< ColorName >& aColors
+                        , std::ostream& aOutput 
+                        , const IntermediateResult& aIResult
+                        , ColorName& aCurrentColor ) const;
         void removeFirstOccurence( const ColorName aColor
-                , std::list< ColorName >& aColors );
+                        , std::list< ColorName >& aColors ) const;
+        void processMarkerForLetter( 
+                        const IntermediateResult::Marker& aMarker
+                        , std::list< ColorName >& aColors
+                        , bool& aReset
+                        , ColorName& aCurrentColor ) const;
+        void updateCurrentColor( std::ostream& aOutput
+                        , bool& aReset
+                        , ColorName& aCurrentColor
+                        , const ColorName& aTopColor ) const;
 
     protected: // fields
         std::list< IRule::Ptr > m_Rules;
