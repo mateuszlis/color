3a5eecb57806072ca2c99a2ee5da0cd4c3a96477
Mateusz Lis <mateusz.lis@gmail.com>
2013-07-03 01:32:19 +0200

    [PROTOTYPE] Implemented timeout for stdin

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/regex.hpp>
+#include <sys/select.h>
 
 #include "colorCommandLine.hxx"
 #include "utils.hh"
@@ -27,7 +28,7 @@ int main( int argc, char* argv[] )
 
         // prototype rules for compilation
         Color::RuleBox::Ptr lRules( new Color::RuleBox );
-        lRules->addRule( Color::IRule::Ptr( new Color::Rule( Color::BOLD_RED, "error", true ) ) );
+        lRules->addRule( Color::IRule::Ptr( new Color::Rule( Color::BOLD_RED, "error", false ) ) );
         Color::NumberRule::Ptr lNumberRule( new Color::NumberRule( Color::LIGHT_GRAY, 2 ) );
         lNumberRule->addColor( Color::RESET );
         lRules->addRule( lNumberRule );
@@ -35,12 +36,24 @@ int main( int argc, char* argv[] )
         lRules->addRule( Color::IRule::Ptr( new Color::Rule( Color::BROWN, "[a-zA-Z_]+\\.cc" ) ) );
         lRules->addRule( Color::IRule::Ptr( new Color::Rule( Color::BOLD_RED, "undefined" ) ) );
         lRules->addRule( Color::IRule::Ptr( new Color::Rule( Color::BOLD_BLUE, "warning" ) ) );
-
         std::string lLine;
-        size_t lI( 0 );
-        while ( std::getline(std::cin, lLine ) )
+        fd_set selectset;
+        struct timeval timeout = {1,0}; //timeout of 10 secs.
+        int ret;
+        FD_ZERO(&selectset);
+        FD_SET(0,&selectset);
+        ret =  select(1,&selectset,NULL,NULL,&timeout);
+        if(ret == 0)
+            return 0;
+              //timeout
+        else if(ret == -1)
+            return 0;
+              //  //error
+        else 
         {
-            std::cout << lRules->process( lLine, lI++ ) << std::endl;
+            size_t lI( 0 );
+            while ( std::getline(std::cin, lLine ) )
+                std::cout << lRules->process( lLine, lI++ ) << std::endl;
         }
         return 0;
     }
