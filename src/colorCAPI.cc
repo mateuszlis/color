a1aeebdad6d482c8c876aac908093ab38a6ab9a2
Mateusz Lis <mateusz.lis@gmail.com>
2013-11-15 22:05:16 +0100

    [FIX] C_API does not reset config every time

--- src/colorCAPI.cc
+++ src/colorCAPI.cc
@@ -4,39 +4,45 @@
 #include "RuleBox.hh"
 #include <stdio.h>
 
-char** g_Argv;
-int g_Argc = 0;
+ColorErrorCode g_Ret;
+Color::RuleBox::Ptr g_AppliedRules;
 
 void color_init( int argc, char** argv )
 {
-    g_Argv = argv;
-    g_Argc = argc;
+    char* lArgs[] = { const_cast< char* >( "color" )
+                   , const_cast< char* >( "syslog" ) }; // default
+    int lArgc = 2;
+    char** lArgv = lArgs;
+
+    try
+    {
+        if ( argc ) // arguments were supported by user
+        {
+            g_Ret = OK;
+            Color::CLHandler lHandler(argc, argv);
+            g_AppliedRules = lHandler.produceRules();
+        }
+        else // use default
+        {
+            g_Ret = INIT_NOT_CALLED_SETTINGS_DEFAULT;
+            Color::CLHandler lHandler(lArgc, lArgv);
+            g_AppliedRules = lHandler.produceRules();
+        }
+    }
+    catch ( const std::exception& e )
+    {
+        puts(e.what());
+        g_Ret = FAILED_TO_PARSE_ARGUMENTS;
+    }
 }
 
 ColorErrorCode color_colorize( char* aCString, char* aDest, size_t aDestSize )
 {
-    char* lArgs[] = { const_cast< char* >( "color" )
-                   , const_cast< char* >( "syslog" ) }; // default
-    // options
-    ColorErrorCode lRet( OK );
     static size_t lLineNum( 0 );
-    char** lArgv;
-    if ( g_Argc )
+    try
     {
-        lArgv = g_Argv;
-    }
-    else
-    {
-        lRet = INIT_NOT_CALLED_SETTINGS_DEFAULT;
-
-        lArgv = lArgs;
-    }
-    try {
-        Color::RuleBox::Ptr lAppliedRules;
-        Color::CLHandler handler( 2, lArgv );
-        lAppliedRules = handler.produceRules();
         std::string lLine( aCString );
-        std::string lRes( lAppliedRules->process( lLine
+        std::string lRes( g_AppliedRules->process( lLine
                                             , lLineNum++ ) );
         if ( lRes.size() > aDestSize )
         {
@@ -50,5 +56,5 @@ ColorErrorCode color_colorize( char* aCString, char* aDest, size_t aDestSize )
         puts(e.what());
         return FAILED_TO_PARSE_ARGUMENTS;
     }
-    return lRet;
+    return g_Ret;
 }
